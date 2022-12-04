ac5d76d19d83bb1631a9ebe587f5fb41f9fb1b6e
noor <noortamer58@gmail.com>
2022-12-04 23:03:50 +0200

    Updating policy and lambda lambda_function

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,14 @@
-import pandas as pd
+from Flask import Flask, render_template
+import os
+ 
+app = Flask(__name__)
 
-def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4], 'col3':[5,6]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.2')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
