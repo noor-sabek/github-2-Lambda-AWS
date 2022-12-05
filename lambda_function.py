cbb742fcc320f9937fc21b9cb99db68a1e80e9ab
noor <noortamer58@gmail.com>
2022-12-05 15:17:57 +0200

    github trigger lambda_function in AWS

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,7 @@
-from Flask import Flask, render_template
-import os
- 
-app = Flask(__name__)
+import pandas as pd
 
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+def lambda_handler(event, context):
+    d = {'col1': [1,2], 'col2': [3,4], 'col3':[5,6]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.2')
