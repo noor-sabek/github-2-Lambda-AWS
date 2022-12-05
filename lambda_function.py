cff3ea9db9f06068dc3c074d80b1ac73a37a6bf6
noor <noortamer58@gmail.com>
2022-12-05 17:14:15 +0200

    adding html file

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,23 @@
-import pandas as pd
+#import pandas as pd
 
-def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4], 'col3':[5,6]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.2')
+# def lambda_handler(event, context):
+    #d = {'col1': [1,2], 'col2': [3,4], 'col3':[5,6]}
+  #  df = pd.DataFrame(data=d)
+   # print(df)
+   # print('Done x1.2')
+ 
+ 
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
